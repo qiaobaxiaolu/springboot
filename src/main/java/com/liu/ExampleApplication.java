7bc5de375ed876dc0771e1ead37aec544940b62d
liujiarui <15229106888@163.com>
2018-03-19 16:28:43 +0800

    异步任务 定时任务

--- src/main/java/com/liu/ExampleApplication.java
+++ src/main/java/com/liu/ExampleApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -20,7 +22,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan(basePackages="com.liu.mapper")
 @EnableAutoConfiguration
 //扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
-//@ComponentScan(basePackages= "com.liu")
+@ComponentScan(basePackages= "com.liu")
+//开启异步任务
+@EnableAsync
+//开启定时任务
+@EnableScheduling
 public class ExampleApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
